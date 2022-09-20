c1c2dde2557f1bc0fe48651c61d69db1bb1e637c
hatem bellaaj <hatem.bellaaj@mdsoft-int.com>
2022-09-20 17:58:16 +0100

    Initial version of categorydb generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/ecommerce/category/config/dbmigrations/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MongoDB database migrations using Mongock.
+ */
+package com.ecommerce.category.config.dbmigrations;
---
+++ src/main/java/com/ecommerce/category/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.ecommerce.category.web.rest.vm;
